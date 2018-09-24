e848ba50a4061cf05d4c791b9d82ba461e3bb857
Villa, Carlos <cvilla8@gatech.edu>
2018-09-24 17:10:57 -0400

    updated nfl_game data to get past plays

--- src/nfl_gamedata.py
+++ src/nfl_gamedata.py
@@ -1,26 +1,53 @@
 import json
 import urllib
 
+class nfl_gameData(object):
+    def __init__(self):
+        self.week_info = {}
+        self.week_info = get_game_week_info()
+        
+    def get_game_week_info(self):
+        url = "http://www.nfl.com/liveupdate/scorestrip/ss.json"
+        info = urllib.request.urlopen(url)
+        infos = json.load(info)["gms"]
+        return(infos)
 
-def get_game_week_info():
-    url = "http://www.nfl.com/liveupdate/scorestrip/ss.json"
-    info = urllib.request.urlopen(url)
-    week_info = json.load(info)["gms"]
-    return(week_info)
-
-def get_game_info(eid):
-    url = "http://www.nfl.com/liveupdate/game-center/%s/%s_gtd.json" % (eid, eid)
-    info = urllib.request.urlopen(url)
-    game_play_info = json.load(info)[eid]["drives"]
-    return game_play_info
+    def get_game_info(self, eid):
+        url = "http://www.nfl.com/liveupdate/game-center/%s/%s_gtd.json" % (eid, eid)
+        info = urllib.request.urlopen(url)
+        game_play_info = json.load(info)[eid]["drives"]
+        return game_play_info
     #print(json.dumps(game_play_info, indent=4, sort_keys=True))
 
-game_info = get_game_info("2018091603")
-d = 1
-print(game_info[str(d)]["plays"])
 
-for i in game_info:
-    print(i["plays"])
-    print(json.dumps(i, indent=4, sort_keys=True))
-    #for j in i:
-       # print(j)
+
+    def get_past_plays(self, team_name):
+        eid = None
+        for game in self.week_info:
+            if team_name.lower() == game['vnn'].lower() or team_name.lower() == game['hnn'].lower():
+                eid = game['eid']
+                
+        if eid == None:
+            return (-1)
+        game_info = get_game_info(str(eid))
+        p=0
+        past_plays= {}
+        for i in game_info:
+            if i == "crntdrv":
+                break
+            for j in game_info[i]["plays"]:
+                string = game_info[i]["plays"][str(j)]["desc"]
+                if string.find("TOUCHDOWN") != -1:
+                    past_plays[p] = string
+                    p=+1
+        return past_plays
+    
+
+    
+
+game_info= nfl_gameData()
+week = game_info.week_info
+
+past_plays = game_info.get_past_plays("falcons")
+if past_plays != -1:
+   print(past_plays)
