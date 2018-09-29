c7dea1305aadf80aed7f8c25862e218a7339b300
drharris <david@gtdavid.com>
2018-09-28 21:43:30 -0400

    Support for refresh_token via command line

--- src/slack_bot.py
+++ src/slack_bot.py
@@ -91,7 +91,7 @@ def handle_command(command, channel):
 
 if __name__ == "__main__":
     if slack_client.rtm_connect(with_team_state=False):
-        print("Starter Bot connected and running!")
+        print("GETSCORE connected and running!")
         # Read bot's user ID by calling Web API method `auth.test`
         starterbot_id = slack_client.api_call("auth.test")["user_id"]
         while True:
